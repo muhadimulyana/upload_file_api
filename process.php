5ea00b73e871f2ba0b168049915c51103ba5a4be
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-09-18 10:37:30 +0700

    add process upload

--- process.php
+++ process.php
@@ -6,33 +6,72 @@ error_reporting(E_ALL);
 
 date_default_timezone_set("Asia/Jakarta"); // Set default time zone
 
+function getBaseUrl() 
+{
+    // output: /myproject/index.php
+    $currentPath = $_SERVER['PHP_SELF']; 
+    // output: Array ( [dirname] => /myproject [basename] => index.php [extension] => php [filename] => index ) 
+    $pathInfo = pathinfo($currentPath); 
+    // output: localhost
+    $hostName = $_SERVER['HTTP_HOST']; 
+    // output: http://
+    $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
+    // return: http://localhost/myproject/
+    return $protocol.'://'.$hostName.$pathInfo['dirname']."/";
+}
+
 //Check if post file is exists
-if(isset($_POST['file'])){
+if(isset($_POST['dir'])){
 
-    $path = 'uploads/';
-    $directories = $_POST['directory'];
+    $path = 'uploads';
+    $directories = $_POST['dir'];
 
-    //create folder
-    $oldmask = umask(0);
-    mkdir($path . '/' . $directories, 0777, true);
-    umask($oldmask);
-    
-    
+    //create folder if exists
+    if(!file_exists($path . '/' . $directories)){
+        $oldmask = umask(0);
+        mkdir($path . '/' . $directories, 0777, true);
+        umask($oldmask);
+    }
+
+    $date = date('y-m-d H:i:s');
+    $filename = '[' . $date . ']' . $_POST['file_name'];
+    $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+    $uploadFile = $path . '/' . $directories . '/' . $filename . '.' . $ext;
+
+    try {
+
+        //move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile);
+        if(!move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)){
+            throw new Exception('File tidak dapat diunggah');
+        } else {
+            chmod($uploadFile, 0777);
+            $url = getBaseUrl() . $uploadFile;
+            $out = [
+                'message' => 'Upload success',
+                'url' => $url
+            ];
+        }
+
+    } catch (Exception $e) {
+        $out = [
+            'message' => 'Upload failed ' . $e->getMessage
+        ];
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($out);
+
+
+    // Looping all files & do upload
+
     // $jumlah = count($_FILES['file']['name']);
-
-    // // Looping all files & do upload
     // for($i=0; $i<$jumlah; $i++){
     //     $filename = $_FILES['file']['name'][$i];
-    //     $uploadFile = $PathDef . $folder . '/' . $filename;
+    //     $uploadFile = $path . '/' . $directories . '/' . $filename;
     //     $upload = move_uploaded_file($_FILES['file']['tmp_name'][$i], $uploadFile);
     //     chmod($uploadFile, 0777);
     // }
 
-    // if($upload){
-    //     exit;
-    // } else {
-    //     echo "error";
-    //     exit;
-    // }
+   
 
 }
